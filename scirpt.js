4d862100f411c0f4394297cd979b586151e8fb72
AkAj0725 <akshadkalaskar2@gmail.com>
2024-09-18 22:59:25 +0530

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,17 @@
+/*cdsjs typing typing animation typed.js */
+//typing animatation
+
+var typed=new Typed(".typing",{
+    strings: ["Student","Programmer","Developer","Problem Solver"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop : true
+})
+
+var typed=new Typed(".typing-2",{
+    strings: ["Student","Programmer","Developer","Problem Solver"],
+    typeSpeed:99,
+    backSpeed:58,
+    loop : true
+})
+
